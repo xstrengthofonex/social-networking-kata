a6fb5c8b9b822b6370b6d821a79b1f7a74dfaaa6
xstrengthofonex <xstrengthofonex@gmail.com>
2019-12-23 19:12:06 +0900

    Removed Response class import

--- social_network/apis/register_user_api.py
+++ social_network/apis/register_user_api.py
@@ -4,11 +4,10 @@ import falcon
 
 from social_network.domain import user
 from social_network.use_cases import register_user
-from social_network.use_cases.register_user import Response
 
 
 class Presenter(register_user.Presenter):
-    def on_success(self, response: Response) -> None:
+    def on_success(self, response: register_user.Response) -> None:
         pass
 
     def on_failure(self, message: str) -> None:
