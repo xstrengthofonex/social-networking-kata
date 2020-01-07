e24a8ae04d8c5498d3cd5516bbde62bd8bc4d541
xstrengthofonex <xstrengthofonex@gmail.com>
2020-01-07 19:25:43 +0900

    Deleted comments from test_create_post_api.py

--- tests/integration/test_create_post_api.py
+++ tests/integration/test_create_post_api.py
@@ -8,12 +8,13 @@ class CreatePostAPITest(dsl.APITest):
     TEXT = "Some text"
 
     def test_create_a_new_post_with_a_registered_user(self):
-        # There seem to be no checks that a user is logged in
-        # I think they decided to forgo sessions and authorization in the requirements
-
         registered_user = self.create_registered_user()
         response = self.create_post(registered_user.id, self.TEXT)
         self.assert_post_created(response, registered_user.id, self.TEXT)
+
+    def test_do_not_create_a_new_post_with_a_missing_user(self):
+        response = self.create_post("unregisteredUser", self.TEXT)
+        self.assert_post_not_created(response)
 
     def create_post(self, user_id: str, text: str) -> webtest.TestResponse:
         create_post_data = dict(user_id=user_id, text=text)
@@ -28,13 +29,6 @@ class CreatePostAPITest(dsl.APITest):
         self.assertIsNotNone(response.json.get("date"))
         self.assertIsNotNone(response.json.get("time"))
 
-    def test_do_not_create_a_new_post_with_a_missing_user(self):
-        # There seem to be no checks that a user is logged in
-        # I think they decided to forgo sessions and authorization in the requirements
-
-        response = self.create_post(None, self.TEXT)
-        self.assert_post_not_created(response)
-        
     def assert_post_not_created(self, response: webtest.TestResponse) -> None:
         self.assertEqual("400 Bad Request", response.status)
         self.assertEqual("application/json", response.content_type)
