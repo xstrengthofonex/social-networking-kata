f924cdd62680947e4a516609bac849ca91a60159
xstrengthofonex <xstrengthofonex@gmail.com>
2020-01-10 17:41:42 +0900

    Set up route for follow_user_api

---
+++ social_network/apis/follow_user_api.py
@@ -0,0 +1,6 @@
+import falcon
+
+
+class Controller:
+    def on_post(self, request: falcon.Request, response: falcon.Response) -> None:
+        pass
--- social_network/app.py
+++ social_network/app.py
@@ -1,7 +1,12 @@
 import falcon
 
 from social_network.repositories import users, posts
-from social_network.apis import register_user_api, login_api, create_post_api, retrieve_timeline_api
+from social_network.apis import (
+    register_user_api,
+    login_api,
+    create_post_api,
+    retrieve_timeline_api,
+    follow_user_api)
 
 
 def create():
@@ -12,4 +17,5 @@ def create():
     api.add_route("/login", login_api.Controller(user_repository))
     api.add_route("/users/{user_id}/posts", create_post_api.Controller(post_repository, user_repository))
     api.add_route("/users/{user_id}/timeline", retrieve_timeline_api.Controller(post_repository, user_repository))
+    api.add_route("/follow", follow_user_api.Controller())
     return api
